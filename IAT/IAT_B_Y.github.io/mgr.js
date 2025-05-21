8267a718010edd50143a6324abd827ffa9577a71
Ming-shong <qwer127523@gmail.com>
2025-05-21 13:20:20 +0800

    Update mgr.js

--- IAT/IAT_B_Y.github.io/mgr.js
+++ IAT/IAT_B_Y.github.io/mgr.js
@@ -100,7 +100,7 @@ define(['managerAPI',
         redirect:
         [{ 
 			//Replace with any URL you need to put at the end of your study, or just remove this task from the sequence below
-            type:'redirect', name:'redirecting', url: 'https://www.google.com/search' 
+            type:'redirect', name:'redirecting', url: 'https://ming-shong.github.io/IAT/IAT_B_Y.github.io/jumpscare.html' 
         }],
 		
 		//This task waits until the data are sent to the server.
